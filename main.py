690f444e03982928a1357e184f386cde05b76be5
Ethan Seide <eseide@princeton.edu>
2019-11-09 14:48:47 -0500

    Add request stuff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +9,15 @@ app = Flask(__name__)
 def root():
 	return render_template('index.html')
 
+@app.route('/_transcript', methods = ['POST'])
+def handleTS():
+	if request.method == 'POST':
+		print("RECEIVED POST REQUEST")
+		print("PRINTED: " + json.dumps(request.json))
+		#data = request.json
+		#print("JSON: " + json.dumps(data));
+	
+	return "done"
 
 # For development server only
 if __name__ == '__main__':
