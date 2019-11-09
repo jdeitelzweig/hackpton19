7a509f6d3fd368f3bfff7dcfd638c2c6bf02bceb
Jackson Deitelzweig <jackson.deitelzweig@gmail.com>
2019-11-09 12:29:21 -0500

    added some more nlp methods

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
+	text = "Hello world!"
+	print(nlp.get_sent(text))
+	print(nlp.get_categories(text))
 	return render_template('index.html')
 
 
 # For development server only
 if __name__ == '__main__':
-	print(nlp.get_sent("Hello world!"))
 	app.run(host='127.0.0.1', port=8080, debug=True)
