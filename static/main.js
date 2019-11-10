6fd7292e8b3b5825c7ec7ee3e6bdd0a3074c43ee
Jackson Deitelzweig <jackson.deitelzweig@gmail.com>
2019-11-10 02:37:11 -0500

    Got line colors changing

--- static/main.js
+++ static/main.js
@@ -3,8 +3,7 @@ var myGraphOne;
 var myGraphTwo;
 var myGraphThree;
 var colorGrad = ["#fffafa","#fef5f5","#fef0f0","#feebeb","#fee6e6","#fde1e1","#fddcdc","#fdd7d7","#fcd2d2","#fccdcd","#fcc8c8","#fbc3c3","#fbbfbe","#fbbab9","#fbb5b4","#fab0af","#faabaa","#faa6a5","#f9a1a0","#f99c9b","#f99796","#f99291","#f88d8c","#f88887","#f88382","#f77e7d","#f77978","#f77473","#f66f6e","#f66a69","#f66564","#f6605f","#f55b5a","#f55655","#f55150","#f44c4b","#f44746","#f44241","#f43e3c","#f33937","#f33432","#f32f2d","#f22a28","#f22523","#f2201e","#f11b19","#f11614","#f1110f","#f10c0a","#f00705"].reverse() +["#fafdfa","#f6fcf5","#f1faf0","#edf8eb","#e8f7e6","#e4f5e2","#dff3dd","#dbf2d8","#d6f0d3","#d2efce","#cdedc9","#c9ebc4","#c4eabf","#c0e8ba","#bbe6b5","#b7e5b1","#b2e3ac","#ade1a7","#a9e0a2","#a4de9d","#a0dc98","#9bdb93","#97d98e","#92d789","#8ed684","#89d480","#85d37b","#80d176","#7ccf71","#77ce6c","#73cc67","#6eca62","#6ac95d","#65c758","#60c553","#5cc44f","#57c24a","#53c045","#4ebf40","#4abd3b","#45bb36","#41ba31","#3cb82c","#38b727","#33b522","#2fb31e","#2ab219","#26b014","#21ae0f","#1dad0a"];
-
-console.log(colorGrad);
+colorGrad = colorGrad.split(",");
 
 function runTimer() {
   clearInterval(myTimer);
@@ -40,6 +39,7 @@ var chart = new CanvasJS.Chart("chartContainer", {
   	},
   	data: [{
   		type: "line",
+      lineThickness: 5,
   		dataPoints: dps
   	}]
   });
@@ -77,9 +77,8 @@ var updateChartPace = function (count) {
   if(diff > max - 1) {
     diff = max - 1;
   }
-  var index = parseInt(diff/max * 100);
-  var box=document.getElementById("paceBar");
-  box.style.backgroundColor=colorGrad[index];
+  var index = Math.floor(diff/max * 100);
+  chart.options.data[0].lineColor = colorGrad[index];
 
 	chart.render();
 };
@@ -101,6 +100,7 @@ var chart = new CanvasJS.Chart("chartContainer3", {
 	},
 	data: [{
 		type: "line",
+    lineThickness: 5,
 		dataPoints: dps
 	}]
 });
@@ -122,9 +122,8 @@ var updateChartSentiment = function (count) {
 		dps.shift();
 	}
 
-  var box=document.getElementById("sentBar");
-  var index = yVal/2 + 50;
-  box.style.backgroundColor=colorGrad[index];
+  var index = Math.floor(yVal/2 + 50);
+  chart.options.data[0].lineColor = colorGrad[index];
 
 	chart.render();
 };
@@ -146,7 +145,7 @@ var chart = new CanvasJS.Chart("chartContainer2", {
 	},
 	data: [{
 		type: "line",
-    color: "red",
+    lineThickness: 5,
 		dataPoints: dps
 	}]
 });
@@ -168,9 +167,12 @@ var updateChartClarity = function (count) {
 		dps.shift();
 	}
 
-  var box=document.getElementById("confBar");
-  var index = Math.floor(yVal * 100)
-  box.style.backgroundColor=colorGrad[index];
+  
+  var index = Math.floor(yVal);
+  if (index > 99) {
+    index = 99;
+  }
+  chart.options.data[0].lineColor = colorGrad[index];
 
 	chart.render();
 };
